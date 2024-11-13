42f383332b841ed11c1d3ce23df369127e32be30
phebe-codedepot <phebe.sawan@codedepot.ch>
2024-11-13 18:36:30 +0800

    Fix test-results api route part 5.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   experimental: {
+    experimental: {
         outputFileTracingIncludes: {
             '/api/test-results': ['./public/**/*']
         }
-   }
+    }
 }
 
 module.exports = nextConfig
